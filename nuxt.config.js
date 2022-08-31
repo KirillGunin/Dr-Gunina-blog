acd58a828a15fdf85917d76b3156f89482dc432e
Gunini <microboss1@gmail.com>
2022-08-31 16:19:12 +0300

    carousel done, AddPostForm/EditPostForm changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/app-components.js'
+    '~plugins/app-components.js',
+    { src: '~plugins/vue-carousel', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
